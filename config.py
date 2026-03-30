fef94e5bd2688fadbeb49f6b4bd3db1f8dc737b5
Naman Sharma <naman262007sharma@gmail.com>
2026-03-31 00:06:44 +0530

    updated config file

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
     JWT_SECRET = os.environ.get("JWT_SECRET", "ztrace_secret_change_in_prod")
-    JWT_EXPIRY_HOURS = 8
+    JWT_EXPIRY_HOURS = 8     
     DEBUG = True
